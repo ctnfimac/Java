eca100f81628092dc4f7fac4fcaa5ca6200fa50d
Christian Peralta <christianipc.1987@gmail.com>
2019-02-21 13:05:40 -0300

    agrego ejercicios 07, sumar numeros de un array

---
+++ Recursividad/07-SumarNumerosDeArray/SumarNumeroDeArray.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class SumarNumeroDeArray {
+
+	public static void main(String Args[]){
+		List<Integer> vector = new ArrayList<Integer>();
+		vector.add(2);
+		vector.add(7);
+		vector.add(1);
+		vector.add(8);
+		vector.add(10);
+		System.out.println("Suma de valores del vector: " + sumarNumeros(vector,vector.size()));
+		System.out.println("Suma de valores del vector con el otro metodo: " + sumarNumeros(vector));
+
+	}
+
+	public static Integer sumarNumeros(List<Integer> v){
+		if(v.size() == 1 ) return v.get(0);
+		Integer value = v.get(0);
+		v.remove(0);
+		return value + sumarNumeros(v);
+	}
+
+	public static Integer sumarNumeros(List<Integer> v, int n){
+		n--;
+		if(n == 0 ) return v.get(n);
+		return v.get(n) + sumarNumeros(v,n);
+	}
+	
+}
